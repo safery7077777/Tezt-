7e0fc313e3494ba5008f6fe7c680dd364c21f31d
safery7077777 <kyverti3@gmail.com>
2026-07-24 10:01:53 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     ADMIN_IDS: str = "8498065644,7727345054"          # Telegram ID админов через запятую
     ADMIN_USERNAMES: str = "Kratz_future, nmproda"    # Юзернеймы админов через запятую (без @)
     START_BALANCE: int = 5000    # Начальный баланс новых игроков
-    MAX_BALANCE: int = 800000000 # Жесткий лимит: 10kk коинов
+    MAX_BALANCE: int = 999999999999999  # Лимит убран (999 триллионов)
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -17,6 +16,6 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-# Предварительный парсинг списков администраторов для быстрого поиска
+# Предварительный парсинг списков администраторов
 ADMIN_ID_LIST = [int(x.strip()) for x in settings.ADMIN_IDS.split(",") if x.strip().isdigit()]
 ADMIN_USERNAME_LIST = [x.strip().lower() for x in settings.ADMIN_USERNAMES.split(",") if x.strip()]
